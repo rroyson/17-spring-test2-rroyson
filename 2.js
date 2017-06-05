c26df6d80b4b7404d02c86f36d1bda38d3f060d8
rroyson <frontiersons@gmail.com>
2017-06-05 18:35:41 -0400

    2.js imerative

---
+++ 2.js
@@ -0,0 +1,11 @@
+//const {slice, charAt, toUpper, join, map,range} = require('ramda')
+
+var myArray = ["black","green","blue","navy","orange","gold","purple"]
+
+
+
+for (var i=0;i< myArray.length;i+=1) {
+   myArray[i] = myArray[i].charAt(0).toUpperCase() + myArray[i].substring(1);
+}
+
+console.log(myArray);
